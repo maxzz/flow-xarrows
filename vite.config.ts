f2ab69e4ba19fecb7c3525cecdb1ac69cb5e6e8d
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-03 16:43:15 -0800

    WIP. vite. remove "Could not Fast Refresh" info message

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
+import { createLogger, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const logger = createLogger();
+const loggerInfo = logger.info;
+
+logger.info = (msg, options) => {
+    if (msg.includes('Could not Fast Refresh')) {
+        return;
+    }
+    loggerInfo(msg, options);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    customLogger: logger,
     server: {
         port: 3000,
-    }
+    },
 });
